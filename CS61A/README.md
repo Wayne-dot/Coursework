3261a2b287b414e7b054d39ddc7ce2aec194cceb
Wayne-dot <wayneting.wc@gmail.com>
2024-07-30 23:15:17 -0400

    hw01 complete

---
+++ CS61A/README.md
@@ -0,0 +1,7 @@
+```
+to test the function
+python3 ok -q {name of the function}
+
+to check the score
+python3 ok --score
+```
--- CS61A/hw01/hw01.py
+++ CS61A/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return (i**2 + j**2 + k**2) - max(i, j, k)**2
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +66,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    
+    for i in range(1, n):
+        if n % i == 0:
+            result = i
+    return result        
 
 
 def hailstone(n):
@@ -88,4 +93,13 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    count = 1
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        elif n % 2 == 1:
+            n = 3*n + 1
+        print(n)
+        count += 1
+    return count

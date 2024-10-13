4bff430ea2eaf56f97757b65f1fbb526ab11aec9
Wayne-dot <wayneting.wc@gmail.com>
2024-10-12 23:39:03 -0400

    setup

---
+++ ESE224/Lab05/shoppingList.h
@@ -0,0 +1,17 @@
+
+
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class ShoppingList{
+    private:
+        string file_name;
+        fstream myFile;
+        string most_expansive_item;
+        string name;
+    public:
+        ShoppingList();
+        
+};
